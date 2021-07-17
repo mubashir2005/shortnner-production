0487e2962f21f9182d582d40b951de40f4c4f1a7
dingus45191 <mubashirhasan716@gmail.com>
2021-07-17 16:23:35 +0530

    ready to publish to play store

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
+const withPwa = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const config = {
+    // other next withPwa
+    async headers() {
+        return [{ source: "/(.*)", headers: createSecureHeaders() }];
+    },
+    future: { webpack5: true },
+    pwa: {
+        disable: process.env.NODE_ENV === "development",
+        dest: "public",
+        register: true,
+    },
+};
+module.exports = withPwa(config);
