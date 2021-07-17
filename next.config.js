a67d529bd17ea9d2dcbcd5266c1e99fcdd29dcd9
dingus45191 <mubashirhasan716@gmail.com>
2021-07-17 16:30:30 +0530

    done everything

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const config = {
     },
     future: { webpack5: true },
     pwa: {
-        disable: process.env.NODE_ENV === "development",
         dest: "public",
         register: true,
     },
